8a74f6e0221c1b9ea8a5a9df2e8555e3dec4b0f4
y-nanbu <y.nanbu802@gmail.com>
2016-06-02 13:49:37 +0900

    import thymeleaf

---
+++ resources/static/js/sample/sample.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('#primary-btn').click(function() {
+//    $.ajax({
+//      url: "/ftlsample/doSave"
+//    }).then(function(data) {
+//      $('.content').text(data.content);
+//    });
+    alert("test");
+  });
+});
